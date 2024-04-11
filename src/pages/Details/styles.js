7289e5a8df69bb19e0c7cc4fc6a748a020a56a73
Juliano <julianoaugusto.xp6@gmail.com>
2024-04-11 19:02:12 -0300

    create RiShutDownLine

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,4 +1,12 @@
 import styled from "styled-components"
+
 export const Container = styled.div`
-    background: red;
+    width: 100%;
+    height: 100vh;
+    
+    display: grid; /*vamos criar o grid para dizer onde cada parte do layout vai se encaixar */
+    grid-template-rows: 105px auto; /*Dentro da aplicação, teremos 2 linhas. Uma para o cabeçalho e a outra para o corpo da pagina*/
+    grid-template-areas: 
+    "header"
+    "content" /*Com o grid-template-areas nós iremos nomear essas áreas para aplicar o grid-template-rows*/
 `
