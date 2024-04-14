f90391903a89566ed6631d36cf485fee7a56d2ed
Juliano <julianoaugusto.xp6@gmail.com>
2024-04-14 12:05:44 -0300

    finished details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
@@ -9,22 +9,34 @@ export function Details() {
   return(
     <Container>
       <Header/>
+      <main>
+        <Content>
+          <ButtonText title='Excluir nota'/>
 
-      <ButtonText title='Excluir nota'/>
+          <h1>
+            Introdução ao React
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis obcaecati tenetur non maiores quasi nemo, nihil, nostrum doloremque beatae ipsam quo sint veritatis accusamus! Culpa perferendis ratione aut reiciendis similique? Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia optio fugiat enim reiciendis nihil aperiam, perspiciatis deleniti dolores alias quod assumenda dolorem veritatis qui nobis porro similique praesentium dolor excepturi!
+          </p>
+          
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">http://www.rocketseat.com.br</a></li>
+              <li><a href="#">http://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+          
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">http://www.rocketseat.com.br</a></li>
-          <li><a href="#">http://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
-      
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="nodejs"/>
-      </Section>
-      
-      <Button title="Voltar"/>
+          <Button title="Voltar"/>
+
+        </Content>
+      </main>
+
     </Container>  
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -8,7 +8,13 @@ export const Container = styled.div`
     grid-template-rows: 105px auto; /*Dentro da aplicação, teremos 2 linhas. Uma para o cabeçalho e a outra para o corpo da pagina*/
     grid-template-areas: 
     "header"
-    "content" /*Com o grid-template-areas nós iremos nomear essas áreas para aplicar o grid-template-rows*/
+    "content"; /*Com o grid-template-areas nós iremos nomear essas áreas para aplicar o grid-template-rows*/
+
+    > main {
+        grid-area: content; /*- usaremos o main para aplicar a região do grid nela */
+        overflow-y: auto; /*- Quando falamos do eixo Y estamos nos referindo ao eixo vertical e o scroll é o método de limitação, se o conteúdo não couber mais na vertical, ele vai habilitar uma barra de rolagem automaticamente*/
+        padding: 64px 0;
+    }
 `;
 
 export const Links = styled.ul`
@@ -21,4 +27,30 @@ export const Links = styled.ul`
             color:  ${({ theme }) => theme.COLORS.WHITE};
         }
     }
+`;
+
+export const Content = styled.div `
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child{
+        align-self: end;
+    }
+
+    > h1 {
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+    }
+
+    > p {
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;
+    }
 `
+
+
