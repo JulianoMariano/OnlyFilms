da5317344c619447def28e57d643b8ead3876891
Juliano <julianoaugusto.xp6@gmail.com>
2024-04-15 19:55:50 -0300

    create filter menu

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,12 +16,43 @@ export const Container = styled.div `
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 `;
 export const Brand = styled.div `
+    grid-area: brand;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    >h1 {
+        font-size: 24px;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
 ` 
 export const Menu = styled.ul `
+    grid-area: menu;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    
+    padding-top: 64px;
+    text-align: center;
+
+    > li {
+        margin-bottom: 24px;
+    }
 ` 
 export const Search = styled.div `
+    grid-area: search;
+    background-color: violet;
 ` 
 export const Content = styled.div `
+    grid-area: content;
+    background-color: blue;
 ` 
 export const NewNote = styled.button `
+    grid-area: newnote;
+    background-color: yellow;
 `
