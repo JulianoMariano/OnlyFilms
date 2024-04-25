026e890e3353ec208e19fb6b84e37e69b24e1855
Juliano <julianoaugusto.xp6@gmail.com>
2024-04-25 19:28:28 -0300

    create register interface

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,47 @@
+import { FiMail, FiLock, FiUser, FiArrowLeft, } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import {  Container, Form, Background } from './styles'
+
+export function SignUp() {
+  return(
+    <Container>
+      
+      <Background />
+
+      <Form>
+        <h1>NodeMovies</h1>
+        <p>Aplicação perfeita para salvar seus filmes favoritos</p>
+        <h2>Crie sua conta</h2>
+
+        <Input 
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+        
+        <Input 
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Cadastrar"/>
+
+        <a 
+          href="#">
+          <FiArrowLeft size={20}/>
+          Voltar para tela de login
+        </a>
+      </Form>
+    </Container>
+  )
+}
