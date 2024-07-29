9692d8f96f007f3f64f2961b1387ff0b74900e5f
Juliano <julianoaugusto.xp6@gmail.com>
2024-07-29 18:05:10 -0300

    integração back+front

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,19 +1,33 @@
 import { useState } from 'react'
 import { FiMail, FiLock, FiUser, FiArrowLeft, } from 'react-icons/fi'
-import {  Container, Form, Background } from './styles'
+import { Container, Form, Background } from './styles'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { api } from '../../services/api'
 
 export function SignUp() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const navigate = useNavigate()
 
   function handleSigUp() {
     if(!name || !email || !password) {
       alert("Atenção, para realizar o cadastro preencha todos os campos")
+      navigate("/")
     }
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+      })
+      .catch(error => {
+        if(error.response) {
+          alert(error.response.data.message)
+        } else{
+          alert("Não foi possível realizar o cadastro")
+        }
+      })
   }
   return(
     <Container>
