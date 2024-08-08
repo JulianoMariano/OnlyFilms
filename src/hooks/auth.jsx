86e63ebaf66f0954e53bb215edfa91a21d6bf61c
Juliano <julianoaugusto.xp6@gmail.com>
2024-08-08 19:10:59 -0300

    create hook authenticator

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,30 @@
 import { createContext, useContext } from "react"
 
+import { api } from "../services/api"
+
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+  async function signIn({ email, password }) {
+
+    try{
+      const response = await api.post("/sessions", { email, password })
+      const { user, token } = response.data
+
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+    } catch(error) {
+      if(error.response) {
+        alert(error.response.data.message)
+        
+      } else {
+        alert("Não foi possível entrar.")
+      }
+    }
+  }
+
   return(
-    <AuthContext.Provider value={{ email: 'juliano@email.com' }}>
+    <AuthContext.Provider value={{ signIn }}>
       { children }
     </AuthContext.Provider>
   )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,20 @@
-import {  Container, Form, Background } from './styles'
-import { Button } from '../../components/Button'
-import { FiMail, FiLock } from 'react-icons/fi'
-import { Input } from '../../components/Input'
+import { useState } from 'react'
 import { Link } from 'react-router-dom' 
 import { useAuth } from '../../hooks/auth'
+import { Input } from '../../components/Input'
+import { FiMail, FiLock } from 'react-icons/fi'
+import { Button } from '../../components/Button'
+import {  Container, Form, Background } from './styles'
 
 export function SignIn() {
-  const data = useAuth()
-  console.log("MEU CONTEXTO =>", data)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const { signIn } = useAuth()
+  
+  function handleSignIn() {
+    signIn({ email, password })
+  }
   
   return(
     <Container>
@@ -20,13 +27,16 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
-        />
+          onChange = {e => setEmail(e.target.value)}
+          />
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange = {e => setPassword(e.target.value)}
         />
-        <Button title="Entrar" />
+
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">
           Criar conta
